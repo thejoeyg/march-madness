303a0a2e2ae1650243a53981d5f18f9155f004d1
Helen Lam <helen.f.lam100@gmail.com>
2015-03-07 18:23:34 -0800

    added before_filter in controller

--- app/controllers/brackets_controller.rb
+++ app/controllers/brackets_controller.rb
@@ -1,6 +1,27 @@
 class BracketsController < ApplicationController
-  def new
 
+  before_filter :authenticate_user!, :except => [:index, :show]
+
+  def index
+    @brackets = Bracket.all
+  end
+
+  def show
+  end
+
+  def edit
+    no = ["id", "created_at", "updated_at"]
+    @column_names = Bracket.columns.map{|c| c.name}
+    @column_names.delete("id")
+    @column_names.delete("created_at")
+    @column_names.delete("updated_at")
+    @teams = Team.all
+
+    @bracket = current_user.bracket
+  end
+
+  def new
+    if current_user.bracket == nil
     no = ["id", "created_at", "updated_at"]
     @column_names = Bracket.columns.map{|c| c.name}
     @column_names.delete("id")
@@ -8,11 +29,14 @@ class BracketsController < ApplicationController
     @column_names.delete("updated_at")
     @teams = Team.all
     @bracket = Bracket.new
+  else
+    redirect_to edit_bracket_path(current_user.bracket.id)
   end
+end
 
   def create
-    byebug
     @bracket = Bracket.new(bracket_params)
+    @bracket.user = current_user
     if @bracket.save
       redirect_to @bracket, notice: "Bracket was successfully created."
     else
@@ -20,8 +44,17 @@ class BracketsController < ApplicationController
     end
   end
 
+  def destroy
+    @bracket.destroy
+    respond_to do |format|
+      format.html { redirect_to brackets_url, notice: 'Bracket was successfully deleted.' }
+    end
+  end
+
   private
   def bracket_params
     params.require(:bracket).permit!
   end
+
+
 end
