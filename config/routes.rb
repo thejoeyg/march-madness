4c0d36d8f66133fab32bdae6b3456eb7adecc0a4
Jordan Stewart <jstewart@weblinc.com>
2016-03-11 21:37:42 -0500

    now it's good

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :brackets
   root "brackets#index"
 
+  resources :teams, only: [:index] 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
